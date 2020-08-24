18d8b2e029e93e0810fa40476bb4ed6a0b1af960
Alberto Osio <osioalberto@gmail.com>
2020-08-24 14:59:44 +0200

    Implemented LayoutSwitch using hooks

--- src/LayoutSwitch.js
+++ src/LayoutSwitch.js
@@ -1,11 +1,25 @@
-import { useContext } from 'react'
-import LayoutContext from './LayoutContext'
+import useBottomDrawer from './useBottomDrawer'
+import useLeftDrawer from './useLeftDrawer'
+import useRightDrawer from './useRightDrawer'
+import useTopDrawer from './useTopDrawer'
 
 export const LayoutSwitch = ({ left, right, top, bottom, children }) => {
-  const ctx = useContext(LayoutContext)
-  const open = ((left && ctx.left) || (right && ctx.right) || (top && ctx.top) || (bottom && ctx.bottom)) > 0
-  const toggle = (left && ctx.toggleLeft) || (right && ctx.toggleRight) || (top && ctx.toggleTop) || (bottom && ctx.toggleBottom)
-  return children({ open, toggle })
+  const { isOpen: isTopOpen, toggle: toggleTop } = useTopDrawer()
+  const { isOpen: isBottomOpen, toggle: toggleBottom } = useBottomDrawer()
+  const { isOpen: isLeftOpen, toggle: toggleLeft } = useLeftDrawer()
+  const { isOpen: isRightOpen, toggle: toggleRight } = useRightDrawer()
+  if (left) {
+    return children({ open: isLeftOpen, toggle: toggleLeft })
+  }
+  if (right) {
+    return children({ open: isRightOpen, toggle: toggleRight})
+  }
+  if (top) {
+    return children({ open: isTopOpen, toggle: toggleTop })
+  }
+  if (bottom) {
+    return children({ open: isBottomOpen, toggle: toggleBottom })
+  }
 }
 
 LayoutSwitch.defaultProps = {
